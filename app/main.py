7fe1f64291a62076183e544d7a5424e795d18a9f
alemonnjy222 <71006485+alemonnjy222@users.noreply.github.com>
2026-03-15 09:49:47 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,25 +1,24 @@
 class Animal:
-    alive = []
+    alive = list["Animal"] = []
 
-    def __init__(self, name: str, health: int) -> None:
+    def __init__(self, name: str, health: int = 100) -> None:
         self.name = name
-        self.health = 100
+        self.health = health
         self.hidden = False
         Animal.alive.append(self)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
 
 class Herbivore(Animal):
-    def hide(self):
+    def hide(self) -> None:
         self.hidden = not self.hidden
 
 class Carnivore(Animal):
-    def bite(self, animal):
+    def bite(self, animal) -> None:
         if isinstance(animal, Herbivore) and not animal.hidden:
             animal.health -= 50
         if animal.health <= 0:
             animal.health = 0
-        if animal in Animal.alive:
             Animal.alive.remove(animal)
 print(Animal.alive)
